634d1a522f11a66e2db94c81fc9485c4eaa90493
Carl-Hugo Marcotte <carl-hugo@smartic.ca>
2015-02-05 11:30:15 -0500

    Boutons Simple utilisant les guards. Setup du "projet" cours 5.

---
+++ Cours05/indexSimpleWhen.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>title</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <link href="css/BoutonSimpleWhen.css" rel="stylesheet" type="text/css"/>
+        
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>flat</h2>
+                    <a href="#" class="btn-bleu-flat">Mon Bouton</a><br><br>
+                    <a href="#" class="btn-rouge-flat">Mon Bouton</a><br><br>
+                    <a href="#" class="btn-vert-flat">Mon Bouton</a><br><br>
+                    <a href="#" class="btn-jaune-flat">Mon Bouton</a><br><br>
+                </div>
+                <div class="col-sm-6">
+                    <h2>normal</h2>
+                    <a href="#" class="btn-bleu">Mon Bouton</a><br><br>
+                    <a href="#" class="btn-rouge">Mon Bouton</a><br><br>
+                    <a href="#" class="btn-vert">Mon Bouton</a><br><br>
+                    <a href="#" class="btn-jaune">Mon Bouton</a><br><br>
+                </div>
+            </div>
+        </div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
